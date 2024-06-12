f07b8779d0655f8ad9013a2c0908fb645951e28b
Thais Oliveira <oliveirath27@gmail.com>
2024-06-12 20:35:34 -0300

    atualizando

--- my-app/src/routes.js
+++ my-app/src/routes.js
@@ -28,13 +28,12 @@ export default function Rotas() {
         }}
       >
         {/* em primeiro apenas para não precisar ficar navegando enquanto desenvolve */}
-        {/* <TabStack.Screen name="teste" component={teste} options={{ headerShown: false }} /> */}
         <TabStack.Screen name="Listas" component={Listas} options={{ headerShown: false }} />
-        {/* <TabStack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <TabStack.Screen name="Home" component={Home} options={{ headerShown: false }} />
         <TabStack.Screen name="Index" component={Index} options={{ headerShown: false }} />
         <TabStack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <TabStack.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} /> */}
-        {/* <TabStack.Screen name="Explorar" component={Explorar} options={{ headerShown: false }} /> */}
+        <TabStack.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} />
+        <TabStack.Screen name="Explorar" component={Explorar} options={{ headerShown: false }} />
         <TabStack.Screen name="Vizualizar" component={Vizualizar} options={{ headerShown: false }} />
       </TabStack.Navigator>
     </NavigationContainer>
